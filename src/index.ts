c5d680981251fa45ee2af872f590773c90cb342b
awank100 <mas.awank100@gmail.com>
2019-12-06 20:59:20 +0700

    fix buyProduct and adjust inquiry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { AxiosInstance } from 'axios'
 import { instance } from './utils/Api'
 
+interface IPpobInquiryArgs {
+  code: string
+  dest: string
+  ref?: string | ''
+  options?: {
+    phone?: string
+    period?: number
+  }
+}
+
+export const PPOB_BPJS_CODE = 'BPJSKES'
+
 export default class MitraReptir {
   public apiKey: string
   public instance: AxiosInstance
@@ -51,7 +63,7 @@ export default class MitraReptir {
     ref?: string | ''
   ): Promise<any> {
     const result = await this.instance.post(
-      '/product',
+      '/transaction',
       {
         type: 'buy',
         code,
@@ -80,25 +92,23 @@ export default class MitraReptir {
     return result.data
   }
 
-  public async ppobInquiry(
-    code: string,
-    dest: string,
-    ref?: string | ''
-  ): Promise<any> {
-    const result = await this.instance.post(
-      '/ppob',
-      {
-        type: 'inquiry',
-        code,
-        dest,
-        ref
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${this.apiKey}`
-        }
+  public async ppobInquiry(args: IPpobInquiryArgs): Promise<any> {
+    const body: any = {
+      type: 'inquiry',
+      code: args.code,
+      dest: args.dest,
+      ref: args.ref
+    }
+
+    if (args.code === PPOB_BPJS_CODE) {
+      body.options = args.options
+    }
+
+    const result = await this.instance.post('/ppob', body, {
+      headers: {
+        Authorization: `Bearer ${this.apiKey}`
       }
-    )
+    })
     return result.data
   }
 
